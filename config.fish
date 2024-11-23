b9ff616f9643dfcc947a963652fcd539a407653b
nibiritis <nibirbiswas410@gmail.com>
2024-11-23 23:08:52 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,14 +2,16 @@ if status is-interactive
     starship init fish | source
 end
 
-fastfetch
+fastfetch --logo Sabotage
 
 # alias
 
 alias ls='eza -x -a --icons'
-alias dnfi='sudo dnf install'
-alias dnfr='sudo dnf remove'
-alias dnfs='sudo dnf search'
+alias sps='sudo pacman -S'
+alias spr='sudo pacman -R'
+alias ss='sudo pacman -s'
 alias anime='fastanime --icons --preview --fzf anilist'
 alias manga='fastanime --manga search -t'
-alias n='nvim
+alias n='nvim'
+alias g='cd GitHub/'
+alias gc='git clone'
